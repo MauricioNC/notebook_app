811421f7feb49728667108a5d591772aa42b4eae
Mauricio NC <mauricionc19@gmail.com>
2025-04-10 21:40:40 -0600

    feat: Updating title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,16 +10,28 @@ class PagesController < ApplicationController
 
   def create
     @notebook = Notebook.find_by(id: params[:notebook_id])
-    @page = @notebook.pages.build(page_params)
+    @page = @notebook.pages.build({title: 'Untitled'})
 
     if @page.save
       redirect_to notebook_page_path(params[:notebook_id], @page.id)
     end
   end
 
+  def update
+    page = Page.find_by(id: params[:id])
+
+    if page.update(page_params)
+      respond_to do |f|
+        f.json do
+          render json: { title: page.title }.to_json
+        end
+      end
+    end
+  end
+
   private
 
   def page_params
-    params.require(:page).permit(:title, :body)
+    params.require(:page).permit(:title, :emoji)
   end
 end
